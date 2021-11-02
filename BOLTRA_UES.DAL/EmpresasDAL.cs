217235246b046fd668feaaac68c9264a836df197
SophieMH891 <sofiamh891@gmail.com>
2021-11-02 00:51:33 -0600

    Clase EmpresasDAL

---
+++ BOLTRA_UES.DAL/EmpresasDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOLTRA_UES.EN;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BOLTRA_UES.DAL
+{
+    public  class EmpresasDAL
+    {
+        public int AgregarEmpresa(EmpresasEN pEmpresa)
+        {
+            BDComun conexion = new BDComun();
+            int retorno;
+            MySqlCommand comando = new MySqlCommand(string.Format("INSERT INTO Empresa (nombre, rubro, descripcion) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}')",
+            pEmpresa.nombre, pEmpresa.rubro, pEmpresa.descripcion), conexion.establecerConxion());
+            retorno = comando.ExecuteNonQuery();
+            try
+            {
+                return retorno;
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show("Ha ocurrido un error inesperado" + e, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return retorno;
+            }
+        }
+    }
+}
